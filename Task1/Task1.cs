5e587dd352f5b44338ab91efd73d2397e45b8a38
Serg1300 <120924062+Serg1300@users.noreply.github.com>
2023-02-26 19:46:42 +0300

    Add files via upload

---
+++ Task1/Task1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Task1
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до папки");
+            string path = Console.ReadLine();
+            var time = DateTime.Now;
+            var di = new DirectoryInfo(path);
+            if (di.Exists)
+            {
+                try
+                {
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        if (time - file.LastAccessTime > TimeSpan.FromMinutes(30))
+                        {
+                            file.Delete();
+                        }
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        if (time - dir.LastAccessTime > TimeSpan.FromMinutes(30))
+                        {
+                            dir.Delete(true);
+                        }
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            }
+            else { Console.WriteLine("Такой папки нет"); }
+        }
+    }
+}
